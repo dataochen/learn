3e7866b9919c6cab52de7805d87ef417848136af
egg_CN <1608384702@qq.com>
2019-12-17 21:57:23 +0800

    1

---
+++ src/test/Test.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * @author dataochen
+ * @Description
+ * @date: 2019/6/4 14:48
+ */
+public class Test {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, InvocationTargetException {
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        String property = System.getProperty("java.net.preferIPv4Stack");
+        System.out.println(property);
+    }
+}
