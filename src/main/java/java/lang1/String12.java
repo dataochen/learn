b27fba731d0e250c490c9f21e9f9ca3070d53261
egg_CN <1608384702@qq.com>
2020-01-14 18:58:07 +0800

    1

---
+++ src/main/java/java/lang1/String12.java
@@ -0,0 +1,17 @@
+package java.lang1;
+
+/**
+ * @author dataochen
+ * @Description
+ * @date: 2020/1/14 18:09
+ */
+public class String12 {
+
+    public static void s() {
+        System.out.println("ss");
+    }
+    public static void main(String12[] args) {
+        String12 string = new String12();
+    }
+}
+
---
+++ src/main/java/java/lang1/Test.java
@@ -0,0 +1,14 @@
+package java.lang1;
+
+/**
+ * @author dataochen
+ * @Description java.lang.SecurityException: Prohibited package name: java.xxx
+ * java不允许包为java.xxx
+ * @date: 2020/1/14 18:50
+ */
+public class Test {
+    public static void main(String[] args) {
+        String s = "";
+        System.out.println(s);
+    }
+}
